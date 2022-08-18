e9043b764a0c4ee75a6e7a87b2efd8c7a65f6d2f
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-18 22:32:55 +0300

    commit tree f81dd20926e7331daacf229e594f6e0a6cbcc456

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,32 @@
 ## code your solution here. 
+ require "pry"
+
+# class Person
+
+#     attr_reader :first_name, :second_name 
+
+#     def name= (full_name)
+#         f_name, s_name = full_name.split
+#         @first_name = f_name
+#         @second_name = s_name
+#     end
+
+#     def name
+#         "#{@first_name} #{@second_name}"
+#     end
+# end
+# oscar = Person.new
+# oscar.name = 'Achieng'
+# puts oscar.name 
+# binding.pry
+
+class Cat
+
+    attr_accessor :name
+    def meow 
+        puts "meow!" 
+        binding.pry
+    end
+end
+rose = Cat.new
+rose.meow
